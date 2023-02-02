1ce02c385623bef2db38e24fbba7e66e7d302a0b
Nikola-Fireside <nikola.sretovic@firesidechat.com>
2023-02-02 18:05:39 +0100

    API tests

---
+++ tests/api/apitest.spec.js
@@ -0,0 +1,20 @@
+const { test, expect } = require('@playwright/test');
+
+test("@API-test > get all users", async ({ request }) => {
+    const _response = await request.get(`https://reqres.in/api/users?page=2`);
+    expect(_response.ok()).toBeTruthy();
+    expect(_response.status()).toBe(200);
+    expect(await _response.text()).toContain('{"id":10,"email":"byron.fields@reqres.in","first_name":"Byron","last_name":"Fields","avatar":"https://reqres.in/img/faces/10-image.jpg"}');
+});
+
+test("@API-test > create new user", async ({ request }) => {
+    const _response = await request.post(`https://reqres.in/api/users`, {
+        data: {
+            name: 'nikola',
+            job: 'playwright',
+        }
+    });
+    expect(_response.ok()).toBeTruthy();
+    expect(_response.status()).toBe(201);
+    expect(await _response.text()).toContain('"name":"nikola"' || '"job":"playwright"' || '"id":');
+});
